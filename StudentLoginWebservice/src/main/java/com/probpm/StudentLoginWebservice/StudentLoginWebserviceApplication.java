7d0fe27871da93669a4f74545fd4090b83353f63
Rajesh <raj.eng2009@gmail.com>
2020-04-06 15:34:15 -0400

    First Commit

---
+++ StudentLoginWebservice/src/main/java/com/probpm/StudentLoginWebservice/StudentLoginWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.probpm.StudentLoginWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoginWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoginWebserviceApplication.class, args);
+	}
+
+}
---
+++ StudentLoginWebservice/src/test/java/com/probpm/StudentLoginWebservice/StudentLoginWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probpm.StudentLoginWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoginWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
